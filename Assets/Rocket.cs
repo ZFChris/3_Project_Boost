8e96998d0f7d9dd7d5e44a8fcf024a7d976f2aa2
ZFChris <35233610+ZFChris@users.noreply.github.com>
2018-02-26 18:26:00 +0100

    good stuff

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,42 +4,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float MainThrust = 100f;
     Rigidbody rigidBody;
     AudioSource audiosource;
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
         audiosource = GetComponent<AudioSource>();
-	}
-	
+	}	
 	// Update is called once per frame
 	void Update () {
         ProccesInput();
 	}
-
     private void ProccesInput()
     {
         Thrust();
         Rotate();
     }
 
-    private void Rotate()
+    private void OnCollisionEnter(Collision collision)
     {
-        if (Input.GetKey(KeyCode.A))
+        switch (collision.gameObject.tag)
         {
-            transform.Rotate(Vector3.forward * 90 * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(Vector3.back * 90 * Time.deltaTime);
+            case "Friendly":
+                print("OK");
+                //do nothing
+                break;
+            case "Bad":
+                print("Dead");
+                break;
+
         }
     }
-
     private void Thrust()
     {
+        float ThrustThisFrame = MainThrust * Time.deltaTime;
+
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * 90 * Time.deltaTime);
+            rigidBody.AddRelativeForce(Vector3.up * ThrustThisFrame);
             if (audiosource.isPlaying == false)
             {
                 audiosource.Play();
@@ -49,5 +53,24 @@ public class Rocket : MonoBehaviour {
         {
             audiosource.Stop();
         }
+    }
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;  //take manual control of rotation
+
+        
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; //resume physics control
+
     }
 }
